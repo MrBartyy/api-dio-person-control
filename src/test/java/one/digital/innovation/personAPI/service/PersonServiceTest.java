e8f715050e26e0e5fcdbb4b1eec0fe0c4a836edb
MrBartyy <barty.alves@gmail.com>
2021-08-25 19:12:07 -0300

    Developed unit test for create person

---
+++ src/test/java/one/digital/innovation/personAPI/service/PersonServiceTest.java
@@ -0,0 +1,52 @@
+package one.digital.innovation.personAPI.service;
+
+import one.digital.innovation.personAPI.dto.mapper.PersonMapper;
+import one.digital.innovation.personAPI.dto.request.PersonDTO;
+import one.digital.innovation.personAPI.dto.response.MessageResponseDTO;
+import one.digital.innovation.personAPI.entities.Person;
+import one.digital.innovation.personAPI.repositories.PersonRepository;
+import one.digital.innovation.personAPI.services.PersonService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digital.innovation.personAPI.utils.PersonUtils.createFakeDTO;
+import static one.digital.innovation.personAPI.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personMapper.toModel(personDTO)).thenReturn(expectedSavedPerson);
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.create(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Person successfully created with ID " + savedPersonId)
+                .build();
+    }
+}
